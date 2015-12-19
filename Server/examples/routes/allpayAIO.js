e1728429e70a2232ce234a8dc093866251935c17
r95222023 <r95222023@gmail.com>
2015-12-18 21:35:12 -0600

    remove the server part.

--- Server/examples/routes/allpayAIO.js
+++
@@ -1,6 +0,0 @@
-var app = require('../lib/expressApp');
-
-app.post('/allpayAIO', function (req, res) {
-    console.log(req.body);
-    res.status(200).send('1|OK');
-});
--- Server/lib/expressApp.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    bodyParser = require('body-parser');
-
-var app=express();
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-module.exports = app;
